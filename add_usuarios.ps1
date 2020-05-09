6ca82498fedfa012d1a369dfff271e15d20e1b09
EduardoSY <61274696+EduardoSY@users.noreply.github.com>
2020-05-09 05:21:39 +0100

    Add files via upload

---
+++ add_usuarios.ps1
@@ -0,0 +1,41 @@
+﻿#Eduardo Da Silva Yanes
+#Script para crear usuarios
+
+Import-Module activedirectory
+
+#Ruta del fichero CSV con los datos de los usuarios  
+$ADUsers = Import-csv C:\Users\Administrador\ScriptsPS\usuarios2.csv
+
+foreach ($User in $ADUsers)
+{
+
+    #Guardamos los datos		
+	$Username 	= $User.username
+	$Password 	= $User.password
+	$Firstname 	= $User.firstname
+	$Lastname 	= $User.lastname
+	$OU 		= $User.ou 
+
+    #Comprobamos si existe el usuario ya
+	if (Get-ADUser -F {SamAccountName -eq $Username})
+	{
+		 #Si existe, obviamente no lo creamos. Anunciamos la situacion
+		 Write-Warning "Ya existe una cuenta con el nombre $Username"
+	}
+	else
+	{
+		
+        #Creamos el usuario
+		New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@asxt04.local" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -DisplayName "$Lastname, $Firstname" `
+            -Path $OU `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $False
+            
+	}
+}
---
+++ grupos_add.ps1
@@ -0,0 +1,25 @@
+﻿#Eduardo Da Silva Yanes
+#Script para crear grupo e introducir los usuarios
+Import-Module activedirectory
+
+$ErrorActionPreference="silentlycontinue" #Silenciar los errores de la busqueda
+
+$ADUsers = Import-csv C:\Users\Administrador\ScriptsPS\ugrupos.csv
+
+foreach ($User in $ADUsers)
+{
+    $Grupo    = $User.group
+    $Username = $User.username
+
+    $Existe = Get-ADGroup -Identity $Grupo #Comprobamos si el grupo existe
+
+    #Si no existe lo creamos
+    if ($Existe -eq $NULL)
+    {
+        Write-Warning "El grupo $Grupo no existe. Este sera creado"
+        New-ADGroup -name $Grupo -GroupScope Global -GroupCategory Security -Path "OU=Aislados,DC=asxt04,DC=local"
+    }
+       
+    Add-ADGroupMember -Identity $Grupo -Members $Username
+
+}
